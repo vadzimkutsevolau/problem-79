b20e99c5c9b3333458305d2f9f4334c579d3f4d8
vadzimkutsevolau <v.kutsevolau@gmail.com>
2021-09-01 18:52:12 +0300

    refactor code

--- problem79.rb
+++ problem79.rb
@@ -1,13 +1,13 @@
 file = File.read("p079_keylog.txt")
-@each_pass_line = file.split("\n").map(&:chars).map {|x| x.map(&:to_i)}
+each_pass_line = file.split("\n").map(&:chars).map {|x| x.map(&:to_i)}
 
 # finding uniq digits
 uniq_digs = file.split("\n").join.chars.uniq.map(&:to_i)
 # [3, 1, 9, 6, 8, 0, 2, 7]
 
-def digs_to_the_left_to_x(x)
+def digs_to_the_left_to_x(x, line)
 	array = []
-		@each_pass_line.map do |line|
+		line.map do |line|
 		if line.include?(x)
 			line.map do |dig|
 				dig == x ? break : array << dig
@@ -19,7 +19,7 @@ end
 
 digs_hash = uniq_digs.map do |x| 
 	uniq_digs_hash = {}
-	uniq_digs_hash[x] =	digs_to_the_left_to_x(x).uniq
+	uniq_digs_hash[x] =	digs_to_the_left_to_x(x, each_pass_line).uniq
 	uniq_digs_hash
 end
 
